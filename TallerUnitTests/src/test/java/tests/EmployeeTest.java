df37c84a9a6ea9e16f58260086170146b20d7e63
RobertoPatino1 <rapatino@espol.edu.ec>
2022-08-24 10:52:08 -0500

    Agregados tests

--- TallerUnitTests/src/test/java/tests/EmployeeTest.java
+++ TallerUnitTests/src/test/java/tests/EmployeeTest.java
@@ -5,6 +5,7 @@
 package tests;
 
 import com.mycompany.tallerpruebas.Employee;
+import com.mycompany.tallerpruebas.EmployeeType;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,66 +18,50 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author Estudiante
  */
 public class EmployeeTest {
+
     
     public EmployeeTest() {
     }
-    /*
-    @org.junit.jupiter.api.BeforeAll
-    public static void setUpClass() throws Exception {
-    }
-
-    @org.junit.jupiter.api.AfterAll
-    public static void tearDownClass() throws Exception {
-    }
-
-    @org.junit.jupiter.api.BeforeEach
-    public void setUp() throws Exception {
-    }
-
-    @org.junit.jupiter.api.AfterEach
-    public void tearDown() throws Exception {
-    }
-    */
-    
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
-    
-    @BeforeEach
-    public void setUp() {
-    }
-    
-    @AfterEach
-    public void tearDown() {
-    }
-    
     
     
     /*
-    Unit test del constructor
+    Test del constructor
     */
+    @org.junit.jupiter.api.Test
+    public void testConstructor(){
+        float salary = 920.0F;
+        String currency= "USD";
+        float bonusPercentage= 0.20F;
+        EmployeeType employeeType=EmployeeType.Manager;
+        
+        Employee empleado = new Employee(salary,currency,bonusPercentage,employeeType);
+        
+        assertNotEquals(null,empleado);
+    }
     
     
     
-    
-    
-
     /**
      * Test of cs method, of class Employee.
      */
+    
+    
+    
     @org.junit.jupiter.api.Test
     public void testCs() {
-        System.out.println("cs");
-        Employee instance = null;
-        float expResult = 0.0F;
-        float result = instance.cs();
-        assertEquals(expResult, result, 0.0);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Employee worker = new Employee(300.0F,"USD",0.15F,EmployeeType.Worker);
+        Employee manager = new Employee(400.0F,"EU",0.15F,EmployeeType.Manager);
+        Employee supervisor = new Employee(500.0F,"USD",0.15F,EmployeeType.Supervisor);
+        float expResult1 = 300.0F;
+        float expResult2 = 380.105F;
+        float expResult3 = 500.0525F;
+        float resultW = worker.cs();
+        float resultM = manager.cs();
+        float resultS = supervisor.cs();
+        assertEquals(expResult1, resultW);
+        assertEquals(expResult2, resultM);
+        assertEquals(expResult3, resultS);
+
     }
 
     /**
@@ -84,13 +69,20 @@ public class EmployeeTest {
      */
     @org.junit.jupiter.api.Test
     public void testCalculateYearBonus() {
-        System.out.println("CalculateYearBonus");
-        Employee instance = null;
-        float expResult = 0.0F;
-        float result = instance.CalculateYearBonus();
-        assertEquals(expResult, result, 0.0);
+        Employee worker = new Employee(400.0F,"EU",0.15F,EmployeeType.Worker);
+        Employee manager = new Employee(750.0F,"USD",0.15F,EmployeeType.Manager);
+        Employee supervisor = new Employee(860.0F,"USD",0.30F,EmployeeType.Supervisor);
+        float expResult1 = 386.0F;
+        float expResult2 = 1136.0F;
+        float expResult3 = 1053.0F;
+        float result1 = worker.CalculateYearBonus();
+        float result2 = manager.CalculateYearBonus();
+        float result3 = supervisor.CalculateYearBonus();
+        assertEquals(expResult1, result1);
+        assertEquals(expResult2, result2);
+        assertEquals(expResult3, result3);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+
     }
     
     
